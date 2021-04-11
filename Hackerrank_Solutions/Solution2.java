dc9f5a94d496409194da794475b5cfe9c68f2df2
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2021-04-11 17:07:34 +0530

    Create Solution2.java

---
+++ Hackerrank_Solutions/Solution2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
