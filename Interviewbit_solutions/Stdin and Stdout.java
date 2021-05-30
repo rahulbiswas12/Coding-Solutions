16215a0969ff4a3794583fa072e555b71f8a6b56
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2021-05-30 09:08:33 +0530

    Add files via upload

---
+++ Interviewbit_solutions/Stdin and Stdout.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n1 = scanner.nextInt();
+    int n2 = scanner.nextInt();
+    scanner.close();
+    System.out.println(n1);
+    System.out.println(n2);
+    }
+}
