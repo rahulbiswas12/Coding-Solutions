d6c8b805161e3b963690a686834e7f62ca5e6901
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2021-06-30 22:09:19 +0530

    Create Insert a Node at the Tail of a Linked List.java

---
+++ Hackerrank_Solutions/Linked List/Insert a Node at the Tail of a Linked List.java
@@ -0,0 +1,75 @@
+// https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+      
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtTail function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        SinglyLinkedListNode new_node = new SinglyLinkedListNode(data);
+        if (head == null){
+            head = new_node;
+            return head;
+        } 
+        
+        SinglyLinkedListNode current_node = head;
+        while (current_node.next != null) {
+            current_node = current_node.next;
+        }
+        
+        current_node.next = new_node;
+        return head;
+        
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
