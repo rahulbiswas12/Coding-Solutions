9bf12f985244ea1f8f04b797decb612fc8295800
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2021-08-01 11:32:45 +0530

    Create Java Subarray.java

---
+++ Hackerrank_Solutions/Java Subarray.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                int sum = 0;
+                for(int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+                if(sum < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
