78c3ac4af36c43ec9a9710ecaf07ea1c6e711f00
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2021-08-03 21:38:13 +0530

    Create Revising the Select Query II.sql

---
+++ Hackerrank_Solutions/SQL/Basic Select/Revising the Select Query II.sql
@@ -0,0 +1,5 @@
+-- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?h_r=next-challenge&h_v=zen
+-- github.com/rahulbiswas12
+
+SELECT NAME FROM CITY 
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000
