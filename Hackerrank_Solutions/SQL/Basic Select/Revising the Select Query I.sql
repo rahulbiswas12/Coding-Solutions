9619fd1e46c9ac515c26520c4a6d5484ce3611a7
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2021-08-03 20:53:40 +0530

    Update Revising the Select Query I.sql

--- Hackerrank_Solutions/SQL/Basic Select/Revising the Select Query I.sql
+++ Hackerrank_Solutions/SQL/Basic Select/Revising the Select Query I.sql
@@ -1,2 +1,6 @@
+-- https://www.hackerrank.com/challenges/revising-the-select-query/problem
+-- https://github.com/rahulbiswas12
+
+
 SELECT * FROM CITY
 WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
