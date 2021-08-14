9f354c762e8cb2840e941f19de52ca4c5a2d46e3
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2021-08-14 14:16:15 +0530

    Update Fahrenheit to Celsius.java

--- CodingNinjas_Solutions/Fahrenheit to Celsius.java
+++ CodingNinjas_Solutions/Fahrenheit to Celsius.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int start = s.nextInt();
+		int end = s.nextInt();
+		int step = s.nextInt();
+		
+		int currentFahrenhietValue = start;
+		while(currentFahrenhietValue <= end){
+			int celsiusValue = (int)((5.0 / 9) * (currentFahrenhietValue - 32));
+			System.out.println(currentFahrenhietValue +"\t" +celsiusValue);
+			currentFahrenhietValue += step;
+		}
+	}
+}
+
 /*
 Sample Input 1:
 0 
@@ -24,21 +42,3 @@ Sample Output 2:
 98  36
 111 43
 */
-
-import java.util.Scanner;
-public class Solution {
-	public static void main(String[] args) {
-		
-		Scanner s = new Scanner(System.in);
-		int start = s.nextInt();
-		int end = s.nextInt();
-		int step = s.nextInt();
-		
-		int currentFahrenhietValue = start;
-		while(currentFahrenhietValue <= end){
-			int celsiusValue = (int)((5.0 / 9) * (currentFahrenhietValue - 32));
-			System.out.println(currentFahrenhietValue +"\t" +celsiusValue);
-			currentFahrenhietValue += step;
-		}
-	}
-}
