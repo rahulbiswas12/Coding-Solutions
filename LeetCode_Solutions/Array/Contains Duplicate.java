8dc9a1e2f1080e374a6dd4598903dbc1fb93f3aa
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2021-09-17 12:38:00 +0530

    Update Contains Duplicate.java

--- LeetCode_Solutions/Array/Contains Duplicate.java
+++ LeetCode_Solutions/Array/Contains Duplicate.java
@@ -14,3 +14,20 @@ class Solution {
         return num;
     }
 }
+
+
+Solution 2 -----------------
+    
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> numbers = new HashSet<>();
+        
+        for (int i=0; i<nums.length; i++)
+        {
+            if (numbers.contains(nums[i]))
+                return true;
+            numbers.add(nums[i]);
+        }
+        return false;
+    }
+}
