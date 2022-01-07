af5248b9a069d43a8aa2b0270e5aaffd5068e567
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2022-01-07 20:15:45 +0530

    Update Maximum Subarray.java

--- LeetCode_Solutions/Array/Maximum Subarray.java
+++ LeetCode_Solutions/Array/Maximum Subarray.java
@@ -1,3 +1,22 @@
+// Solution 1
+
+class Solution {
+    public int maxSubArray(int [] nums) {
+        int sum = 0;
+        int maxi = nums[0];
+        for(int i = 0; i<nums.length;i++) {
+            sum += nums[i];
+            if(sum > maxi) maxi = sum;
+            if(sum < 0) sum =0;
+        }
+        return maxi;
+    }
+}
+
+//  https://www.youtube.com/watch?v=w_KEocd__20
+
+// Solution 2
+
 class Solution {
     public int maxSubArray(int a[]) {
         int maxSum = 0;
