7f4e1f7e29aa6f842a5ba46c9bfbeb10c4595c37
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2022-01-13 13:18:57 +0530

    Update BinarySearch.java

--- DSA/BinarySearch.java
+++ DSA/BinarySearch.java
@@ -1,3 +1,29 @@
+class Solution {
+   int binarysearch(int arr[], int n, int k){
+       
+       int start = 0;
+       int end = n-1;
+       
+       while(start<=end){
+           int mid = (start+end)/2;
+           
+           if(arr[mid] == k) return mid;
+           
+           else if(k > arr[mid] ){
+               start = mid+1;
+           }
+           
+           else{
+               end = mid;
+           }
+       }
+       
+       return -1;
+   }
+}
+
+-----------------------------------------------------------------------------------
+    
 import java.util.Scanner;
 class BinarySearch { 
     int search(int arr[], int x) 
