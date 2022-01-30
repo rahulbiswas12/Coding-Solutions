d7b24c42bb1e60033c926136e0ce9d3b9a9eaef6
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2022-01-30 13:17:14 +0530

    Update Find Quadrant of the coordinate point.java

--- CodingNinjas_Solutions/Find Quadrant of the coordinate point.java
+++ CodingNinjas_Solutions/Find Quadrant of the coordinate point.java
@@ -14,24 +14,40 @@ Output 1 :
 1st Quadrant
 */
 
-import java.util.*;
-public class Solution
-{
-	public static void main(String[] args) 
-	{
+import java.util.Scanner;
+class Solution {	
+	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		int a,b;
-		a = sc.nextInt();
-		b = sc.nextInt();
-		if(a > 0 && b > 0)
-			System.out.print("1st Quadrant");
-		else if(a < 0 && b > 0)
-			System.out.print("2nd Quadrant");
-		else if(a < 0 && b < 0)
-			System.out.print("3rd Quadrant");
-		else if(a > 0 && b < 0)
-			System.out.print("4th Quadrant");
-		else
-			System.out.print("Origin");
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if(x > 0 && y > 0) {
+			System.out.println("1st Quadrant");
+		}
+		
+		else if(x < 0 && y > 0) {
+			System.out.println("2nd Quadrant");
+		}
+		
+		else if(x < 0 && y < 0) {
+			System.out.println("3rd Quadrant");
+		}
+		
+		else if(x > 0 && y < 0) {
+			System.out.println("4th Quadrant");
+		}
+		
+		else if(x == 0 && y == 0) {
+			System.out.println("Origin");
+		}
+		
+		else if(x == 0 && y != 0) {
+			System.out.println("y axis");
+		}
+		
+		else {
+			System.out.println("x axis");
+		}
+		   
 	}
 }
