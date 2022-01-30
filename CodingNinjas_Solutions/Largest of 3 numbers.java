d6687acc9090ab15a8f4e5069387c0b760ffa8e0
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2022-01-30 13:07:13 +0530

    Update Largest of 3 numbers.java

--- CodingNinjas_Solutions/Largest of 3 numbers.java
+++ CodingNinjas_Solutions/Largest of 3 numbers.java
@@ -28,3 +28,19 @@ class Solution {
 		System.out.println(largest);	
 	}
 }
+
+-----------------------------------------------------------------------------------------
+import java.util.Scanner;
+class Solution {	
+	public static void main(String args[]) {		
+		Scanner sc = new Scanner(System.in);		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();		
+		int largest;		
+		if(a >= b && a >= c) largest = a;		
+		else if(b >= a && b >= c) largest = b;
+		else largest = c;
+		System.out.println(largest);	
+	}
+}
