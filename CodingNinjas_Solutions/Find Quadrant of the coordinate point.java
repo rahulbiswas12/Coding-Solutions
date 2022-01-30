5674dea5f96543ab8d03f74140eea54de10777aa
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2022-01-30 15:41:49 +0530

    Update Find Quadrant of the coordinate point.java

--- CodingNinjas_Solutions/Find Quadrant of the coordinate point.java
+++ CodingNinjas_Solutions/Find Quadrant of the coordinate point.java
@@ -15,36 +15,30 @@ Output 1 :
 */
 
 import java.util.Scanner;
-class Solution {	
-	public static void main(String args[]) {
+class Solution {
+	public static void main(String args[]) {		
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
-		int y = sc.nextInt();
-		
+		int y = sc.nextInt();	
+        
 		if(x > 0 && y > 0) {
 			System.out.println("1st Quadrant");
 		}
-		
 		else if(x < 0 && y > 0) {
 			System.out.println("2nd Quadrant");
 		}
-		
 		else if(x < 0 && y < 0) {
 			System.out.println("3rd Quadrant");
 		}
-		
 		else if(x > 0 && y < 0) {
 			System.out.println("4th Quadrant");
 		}
-		
 		else if(x == 0 && y == 0) {
 			System.out.println("Origin");
-		}
-		
+        }
 		else if(x == 0 && y != 0) {
 			System.out.println("y axis");
 		}
-		
 		else {
 			System.out.println("x axis");
 		}
