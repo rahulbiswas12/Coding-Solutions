b711b7bef4dd8e4443506d4146bd9c2d43cfb4f8
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2022-01-31 09:28:25 +0530

    Update Sum of Even Numbers till N.java

--- CodingNinjas_Solutions/Sum of Even Numbers till N.java
+++ CodingNinjas_Solutions/Sum of Even Numbers till N.java
@@ -13,6 +13,19 @@ public class Solution {
 		System.out.println(sum);
 	}
 }
+
+/*
+Input Format :
+Integer N
+Output Format :
+Required Sum 
+
+Sample Input 1 :
+ 6
+Sample Output 1 :
+12
+*/
+
 ______________________________________________________________
 
 public class Solution {
@@ -29,13 +42,31 @@ public class Solution {
 
 --------------------------------------------------------------
 /*
+You have been given a number 'N'. Your task is to find the sum of all even numbers from 1 to 'N' (both inclusive).
+Example :
+Given 'N' : 6
+Sum of all even numbers till 'N' will be : 2 + 4 + 6 = 12
 Input Format :
-Integer N
-Output Format :
-Required Sum 
+The first line of the input contains an integer 'T', denoting the number of test cases.
 
+The first line and the only line of each test case contains an integer 'N'.
+Output Format :
+For each test case, print a single integer representing the sum of even numbers till 'N'.
+
+Print a single line for each test case.
+Note:
+You do not need to print anything, it has already been taken care of. Just implement the given function.
+Constraints :
+1 <= T <= 10
+1 <= N <= 10^5
+
+Time Limit: 1 sec
 Sample Input 1 :
- 6
+2
+6
+2
 Sample Output 1 :
 12
+2
+
 */
