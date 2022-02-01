50d65e8f72c029ce380ab06fed21e9f7e954cfea
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2022-02-01 09:13:44 +0530

    Create Count pairs with given sum in the array.java

---
+++ CodingNinjas_Solutions/Count pairs with given sum in the array.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Example {
+public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ; i < n ; i++) {
+        arr[i] = sc.nextInt();
+    }
+    
+        int sum = sc.nextInt();
+        int count = 0; // Initialize result
+    
+        // Consider all possible pairs and check their sums
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (arr[i] + arr[j] == sum)
+                    count++;
+        System.out.println("Count of pairs is " + count);
+
+
+}
+}
+
+/*
+Input:
+5 
+1 1 2 2 3
+3
+
+
+Output:
+Count of pairs is 3
+*/
